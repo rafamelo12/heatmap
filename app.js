4a414b4c08afd12aa0e6c1880ba3c96e8c92d238
rafamelo12 <rafamelo.oliveira@gmail.com>
2014-11-15 05:35:18 -0500

    Server working

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 /*jshint node:true*/
 
 // app.js
-// This file contains the server side JavaScript code for your application.
-// This sample application uses express as web application framework (http://expressjs.com/),
-// and jade as template engine (http://jade-lang.com/).
 
 var express = require('express');
-
+var ibmdb = require('ibm_db');
+var fs = require('fs');
+var path = require('path')
 // setup middleware
 var app = express();
 app.use(app.router);
@@ -14,11 +13,6 @@ app.use(express.errorHandler());
 app.use(express.static(__dirname + '/public')); //setup static public directory
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views'); //optional since express defaults to CWD/views
-
-// render index page
-app.get('/', function(req, res){
-	res.render('index');
-});
 
 // There are many useful environment variables available in process.env.
 // VCAP_APPLICATION contains useful information about a deployed application.
@@ -30,7 +24,72 @@ var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
 // the document or sample of each service.
 var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
 // TODO: Get service credentials and communicate with bluemix services.
+if(process.env.VCAP_SERVICES){
+	var env = JSON.parse(process.env.VCAP_SERVICES);
+	var db2 = env['sqldb'][0].credentials;
+}
+var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
+// ROUTES
+app.get('/test', function(req, res){
 
+	ibmdb.open(connString, function(err, conn){
+		if(err){
+			res.send("Error occurred: ", err.message);
+		}else{
+			var query = 'SELECT * FROM TESTDATA';
+			conn.query(query, function(err, tables, moreResultsSets){
+				if(!err){
+					var data = [];
+					var count = 0;
+					var key2;
+					var filePath = path.join(__dirname, '/public/test.js');
+					var wstream = fs.createWriteStream(filePath);
+					wstream.write('var quakePoints = [\n');
+					for (var key in tables){
+						key2 = key;
+						key2++;
+						if(!tables[key2]){
+							data[count] = '['+tables[key]['X']+','+tables[key]['Y']+','+tables[key]['INTENSITY']+']\n';
+							wstream.write(data[count]);
+						}else{
+							data[count] = '['+tables[key]['X']+','+tables[key]['Y']+','+tables[key]['INTENSITY']+'],\n';
+							wstream.write(data[count]);	
+						}
+						count++;
+					}
+					wstream.write('];');
+					wstream.end(function (){
+						res.render('test.ejs', {
+								'data': filePath.toString()
+							});
+					});
+					// var filePath = path.join(__dirname, 'public/test.txt');
+					
+
+					
+						// if(!err){
+						// 	res.render('test.ejs', {
+						// 		'data': 'SAVED'
+						// 	});
+						// }else{
+						// 	res.render('test.ejs', {
+						// 		'data': err
+						// 	})
+						// }
+					// res.json(tables);
+					// res.render('test.ejs',{
+					// 	'data': tables
+					// });
+				}else{
+					res.send("Error occurred: ", err.message);		
+				}
+				conn.close(function(){
+					console.log("Connection closed");
+				});
+			});
+		}
+	});
+});
 // The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
 var host = (process.env.VCAP_APP_HOST || 'localhost');
 // The port on the DEA for communication with the application:
--- instructions.md
+++ instructions.md
@@ -5,7 +5,7 @@ Welcome to your new Node.js app!
 Node.js app runs on a server-side high performance, highly scalable, event-driven environment with non-blocking I/O that is programmed with the JavaScript programming language.
 
 1. [Install the cf command-line tool](https://www.ng.bluemix.net/docs/#starters/BuildingWeb.html#install_cf).
-2. [Download the starter application package](https://ace.ng.bluemix.net:443/rest/../rest/apps/9363b60c-05e9-4ab6-a54a-0acea3b98f59/starter-download).
+2. [Download the starter application package](https://ace.ng.bluemix.net:443/rest/../rest/apps/86cfe813-f55a-42e6-b314-225e6640d77a/starter-download).
 3. Extract the package and `cd` to it.
 4. Connect to Bluemix:
 
